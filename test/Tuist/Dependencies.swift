bcc9e487df84ae7cc9d23549e4dd1632c825d38a
choidam <37874238+choidam@users.noreply.github.com>
2022-08-25 16:47:41 +0900

    라이브러리 dynamic 처리

--- test/Tuist/Dependencies.swift
+++ test/Tuist/Dependencies.swift
@@ -8,10 +8,12 @@
 import ProjectDescription
 
 let dependencies = Dependencies(
-    carthage: [],
-    swiftPackageManager: [
-        .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .upToNextMajor(from: "5.0.1")),
-        .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "3.0.0")),
-    ],
-    platforms: [.iOS]
-)
+     swiftPackageManager: .init(
+         [
+            .remote(url: "https://github.com/SnapKit/SnapKit", requirement: .upToNextMajor(from: "5.0.1")),
+            .remote(url: "https://github.com/devxoul/Then", requirement: .upToNextMajor(from: "3.0.0")),
+         ],
+         productTypes: ["Then": .framework]
+     ),
+     platforms: [.iOS]
+ )
