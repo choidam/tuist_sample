fbf05b41bc9f73cd077035af85a5594f40958f2d
choidam <37874238+choidam@users.noreply.github.com>
2022-08-30 15:04:56 +0900

    Home 테스트코드 작성

--- test/Projects/Home/Tests/HomeTests.swift
+++ test/Projects/Home/Tests/HomeTests.swift
@@ -10,6 +10,7 @@ import XCTest
 import RxSwift
 import RxTest
 import RxNimble
+import Nimble
 
 @testable import Home
 
@@ -26,5 +27,22 @@ class HomeTests: XCTestCase {
         scheduler = TestScheduler(initialClock: 0)
         disposeBag = DisposeBag()
     }
-
+    
+    func testCount() throws {
+        scheduler
+            .createHotObservable([
+                .next(1, HomeViewReactor.Action.buttonTap),
+                .next(2, HomeViewReactor.Action.buttonTap)
+            ])
+            .bind(to: reactor.action)
+            .disposed(by: disposeBag)
+        
+        expect(self.reactor.state.map { $0.count })
+            .events(scheduler: scheduler, disposeBag: disposeBag)
+            .to(equal([
+                .next(0, 0),
+                .next(1, 1),
+                .next(2, 2)
+            ]))
+    }
 }
